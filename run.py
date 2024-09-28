6ed876e428bb236925eac3cfcf26d9e0fc627e44
Ashley Preece <ashleyjph@gmail.com>
2024-09-28 12:59:48 +0000

    Add display instructions screen function

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
 
 
+def display_instructions_screen():
+    while True:
+        print("How to play")
+        print("Play Game - p")
+        print("Back to main menu - h")
+        player_choice = input()
+
+        if player_choice == "h":
+            homescreen()
+            break
+        elif player_choice == "p":
+            # start_game()
+            print("> Start Game")
+            break
+        else:
+            print("> Invalid input. Please try again.")
+
+
 def homescreen():
     """
     Display homescreen when program is run
@@ -11,14 +29,15 @@ def homescreen():
         player_choice = input()
         
         if player_choice == "i":
-            # display_instructions_screen()
-            print("> Go to instructions screen")
+            display_instructions_screen()
             break
         elif player_choice == "p":
             # start_game()
-            print("> Go to homescreen")
+            print("> Start Game")
             break
         else:
             print("> Invalid input. Please try again.")
 
 homescreen()
+
+
