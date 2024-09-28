a37456e03a0bdf2673a662c3be4fa5952cc917e4
Ashley Preece <ashleyjph@gmail.com>
2024-09-28 12:40:11 +0000

    Add homescreen function

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def homescreen():
+    """
+    Display homescreen when program is run
+    """
+    while True:
+        print("Dark Castle lite")
+        print("Play Game - p")
+        print("Instructions - i")
+        player_choice = input()
+        
+        if player_choice == "i":
+            # display_instructions_screen()
+            print("> Go to instructions screen")
+            break
+        elif player_choice == "p":
+            # start_game()
+            print("> Go to homescreen")
+            break
+        else:
+            print("> Invalid input. Please try again.")
+
+homescreen()
